e3eef992a3afd3947d3258ef760f1e3dafae90e2
anCRONIK <ancronik@gmail.com>
2022-04-05 20:31:28 +0200

    changed json parsing

--- src/integration-test/java/net/ancronik/samples/cucumber/acceptance/steps/ActuatorHealthCheckSteps.java
+++ src/integration-test/java/net/ancronik/samples/cucumber/acceptance/steps/ActuatorHealthCheckSteps.java
@@ -1,30 +1,26 @@
 package net.ancronik.samples.cucumber.acceptance.steps;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.datatable.DataTable;
+import io.cucumber.gherkin.internal.com.eclipsesource.json.JsonObject;
 import io.cucumber.java.en.And;
 import net.ancronik.samples.cucumber.acceptance.AbstractSteps;
-
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ActuatorHealthCheckSteps extends AbstractSteps {
 
-    ObjectMapper mapper = new ObjectMapper();
-
     @And("^the client receives dependencies health statuses$")
-    public void checkReceivedMeasurementUnits(DataTable table) throws JsonProcessingException {
+    public void checkReceivedMeasurementUnits(DataTable table) {
         String body = latestResponse.getBody();
 
-        Map<String, Object> map = mapper.readValue(body, Map.class);
+        JsonObject jsonData = JsonObject.readFrom(body);
 
         table.entries().forEach(e -> {
-            Map c = (Map) map.get("components");
-            Map h = (Map) c.get(e.get("dependency_name"));
 
-            assertEquals(e.get("status"), h.get("status"));
+            JsonObject c = jsonData.get("components").asObject();
+            JsonObject h = c.get(e.get("dependency_name")).asObject();
+
+            assertEquals(e.get("status"), h.getString("status", null));
         });
     }
 }
